260438094ef0e74d2f47a572912b2e8ee2b4832e
Runald D'souza <runald@gmail.com>
2020-09-27 22:29:23 +0530

    add new field 'lastupdatedon'

--- shared/src/main/scala/com/model/Schema.scala
+++ shared/src/main/scala/com/model/Schema.scala
@@ -9,11 +9,12 @@ trait Schema {
 class Electronics extends Schema {
   override def load: StructType = {
     StructType(Seq(
-      StructField("id", StringType),
+      StructField("id", StringType, nullable = false),
       StructField("name", StringType),
-      StructField("code", StringType),
+      StructField("code", StringType, nullable = false),
       StructField("type", StringType),
       StructField("brand", StringType),
+      StructField("lastupdatedon", LongType, nullable = false),
       StructField("links", ArrayType(StructType(Seq(StructField("type", StringType), StructField("link", StringType))))),
       StructField("details", MapType(StringType, StringType, valueContainsNull = true))
     ))
